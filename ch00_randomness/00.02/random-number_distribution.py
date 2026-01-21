c13b9b30d811b7181d11325cbda083024e5ac5ce
tabreturn <github@tabreturn.com>
2026-01-21 13:42:03 +1100

    - complete 00.02

--- ch00_randomness/00.02/random-number_distribution.py
+++ ch00_randomness/00.02/random-number_distribution.py
@@ -1,1 +1,29 @@
-# placeholder
+# https://natureofcode.com/random/#example-02-a-random-number-distribution
+
+TOTAL = 20  # The total number of counts.
+
+
+def setup():
+    global random_counts
+    size(640, 240)
+
+    # An array to keep track of how often random numbers are picked.
+    random_counts = [0] * TOTAL
+
+
+def draw():
+    global random_counts
+    background(255)
+
+    # Pick a random number and increase the count.
+    index = floor(random(len(random_counts)))
+    random_counts[index] += 1
+
+    stroke(0)
+    stroke_weight(2)
+    fill(127)
+    w = width / len(random_counts)
+
+    # Graph the results.
+    for x, count in enumerate(random_counts):
+        rect(x * w, height - count, w - 1, count)
