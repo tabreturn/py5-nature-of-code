4fd2a1884a14c25a59d54e3108e78e4d292e887c
tabreturn <github@tabreturn.com>
2026-01-21 13:15:37 +1100

    - finalize 00.01 formatting

--- ch00_randomness/00.01/traditional_random_walk.py
+++ ch00_randomness/00.01/traditional_random_walk.py
@@ -1,5 +1,6 @@
 # https://natureofcode.com/random/#the-random-walker-class
 
+
 class Walker:
 
     # Objects have a constructor where they are initialized.
@@ -14,30 +15,30 @@ class Walker:
 
     def step(self):
         # 0, 1, 2, or 3. The random choice determines the step.
-
-        # Four possible steps.
          match floor(random(4)):
+             # Four possible steps.
              case 0: self.x += 1
              case 1: self.x -= 1
              case 2: self.y += 1
              case 3: self.y -= 1
 
-        # Eight possible steps.
-        # Yields -1, 0, or 1
+#        # Eight possible steps.
+#        # Yields -1, 0, or 1
 #        self.x += random_int(-1, 1)
 #        self.y += random_int(-1, 1)
-        # Instead use random() for any floating-point number from -1 to 1.
+#        # Instead use random() for any floating-point number from -1 to 1.
 
 
 # Remember how p5.js works? setup() is executed once when the sketch starts.
 def setup():
     global walker  # A Walker object.
     size(640, 240)
-    walker = Walker()
+    walker = Walker()  # Create the walker.
     background(255)
 
 
 # Then draw() loops forever and ever (until you quit).
 def draw():
-    walker.step()  # Call functions on the walker.
+    # Call functions on the walker.
+    walker.step()
     walker.show()
