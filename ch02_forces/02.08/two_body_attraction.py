f6f7d54ced51cd36e7caa00d95066f3964214071
tabreturn <github@tabreturn.com>
2026-01-27 10:33:55 +1100

    - wip (02.08)

--- ch02_forces/02.08/two_body_attraction.py
+++ ch02_forces/02.08/two_body_attraction.py
@@ -1,3 +1,27 @@
 # https://natureofcode.com/forces/#example-28-two-body-attraction
 
+from body import Body
 
+G = 1.0  # A gravitational constant (for global scaling)
+
+
+def setup():
+    global body_a, body_b
+    size(640, 240)
+
+    # Create two Body objects, A and B.
+    body_a = Body(320, 40, 8)
+    body_b = Body(320, 200, 8)
+
+
+def draw():
+    background(255)
+
+    # A attracts B, and B attracts A.
+    body_a.attract(body_b, G)
+    body_b.attract(body_a, G)
+
+    body_a.update()
+    body_a.show()
+    body_b.update()
+    body_b.show()
