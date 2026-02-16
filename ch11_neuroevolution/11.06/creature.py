9e3de2ff22bea60ba4a05b9c17936a63424154e3
tabreturn <github@tabreturn.com>
2026-02-16 20:50:18 +1100

    - scaffold 11.06 (copy over 11.05 files)

---
+++ ch11_neuroevolution/11.06/creature.py
@@ -0,0 +1,50 @@
+# PY5 IMPORTED MODE CODE
+
+from sensor import Sensor
+
+
+class Creature:
+
+    def __init__(self, x: float = 0.0, y: float = 0.0):
+        # The creature has a position and radius.
+        self.position = Py5Vector2D(x, y)
+        self.r = 16
+
+        # The creature has a list of sensors.
+        self.total_sensors = 15  # How about 15 sensors?
+        # Create the sensors for the creature.
+        self.sensors = [
+          Sensor(
+            Py5Vector2D
+              # First, calculate a direction for the sensor.
+              .from_heading(remap(i, 0, self.total_sensors, 0, TAU))
+              # Create a vector a little bit longer than radius as the sensor.
+              .set_mag(self.r * 2)
+          )
+          for i in range(self.total_sensors)
+        ]
+
+    def sense(self, food: 'Food') -> None:
+        """Call the sense() method for each sensor."""
+
+        for sensor in self.sensors:
+            sensor.sense(self.position, food)
+
+    def show(self) -> None:
+        """Draw the creature and all the sensors."""
+
+        push()
+        translate(*self.position)
+
+        for sensor in self.sensors:
+            stroke(0)
+            line(0, 0, sensor.v.x, sensor.v.y)
+            if sensor.value > 0:
+                fill(255, sensor.value * 255)
+                stroke(0, 100)
+                circle(sensor.v.x, sensor.v.y, 8)
+
+        no_stroke()
+        fill(0)
+        circle(0, 0, self.r * 2)
+        pop()
--- ch11_neuroevolution/11.06/neuroevolutionary_ecosystem.py
+++ ch11_neuroevolution/11.06/neuroevolutionary_ecosystem.py
@@ -1,2 +1,31 @@
 # https://natureofcode.com/neuroevolution/#learning-from-the-sensors
 
+from creature import Creature
+from food import Food
+
+
+def setup():
+    global bloop, food
+    size(640, 240)
+
+    # One bloop, one piece of food.
+    bloop = Creature()
+    food = Food()
+
+    # Overwrite food attributes to match p5.js example.
+    food.position = Py5Vector2D(width / 2, height / 2)
+    food.r = 32
+
+
+def draw():
+    background(255)
+
+    # Temporarily control the bloop with the mouse.
+    bloop.position.x, bloop.position.y = mouse_x, mouse_y
+
+    # Draw the food and the bloop.
+    food.show()
+    bloop.show()
+
+    # The bloop senses the food.
+    bloop.sense(food)
