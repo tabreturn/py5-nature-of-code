8ede200609087ff8c648bb37e846388c2149c104
tabreturn <github@tabreturn.com>
2026-02-20 10:04:38 +1100

    - finalise 07.02

--- ch07_cellular_automata/07.02/game_of_life.py
+++ ch07_cellular_automata/07.02/game_of_life.py
@@ -6,7 +6,7 @@ W = 8
 def setup():
     global board, columns, rows
     size(640, 240)
-    
+
     columns = width // W
     rows = height // W
 
@@ -14,6 +14,7 @@ def setup():
 
     for i in range(1, columns - 1):
         for j in range(1, rows - 1):
+            # Start each cell with a 0 or 1.
             board[i][j] = random_int()  # Calculate the state for each cell.
             # With no arguments, random_int() return zero or one.
 
@@ -41,7 +42,7 @@ def draw():
             if board[i][j] == 1 and neighbor_sum < 2:
                 next_[i][j] = 0
             # If cell alive with > 3 live neighbors, it dies from overpopulation.
-            elif board[i][j] == 1 and neighbor_sum >  3:
+            elif board[i][j] == 1 and neighbor_sum > 3:
                 next_[i][j] = 0
             # If cell dead with 3 live neighbors (exactly), it is born!
             elif board[i][j] == 0 and neighbor_sum == 3:
@@ -62,7 +63,4 @@ def draw():
 
 
 def create_2d_array(columns: int, rows: int) -> list[list[int]]:
-    return [
-      [0 for _ in range(rows)]
-      for _ in range(columns)
-    ]
+    return [[0 for _ in range(rows)] for _ in range(columns)]
