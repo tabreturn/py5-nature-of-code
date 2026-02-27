bde04fd952e797388c62f63d0b6118825ad47bde
tabreturn <github@tabreturn.com>
2026-02-27 21:21:26 +1100

    - complete 06.11

--- ch06_physics_libraries/06.11/simple_spring_toxiclibs.py
+++ ch06_physics_libraries/06.11/simple_spring_toxiclibs.py
@@ -19,7 +19,8 @@ def setup():
     global physics, particle_1, particle_2
     size(640, 240)
 
-    physics = VerletPhysics2D()  # Create a Toxiclibs world.
+    # Create a Toxiclibs.js Verlet physics world.
+    physics = VerletPhysics2D()
     physics.setWorldBounds(Rect(0, 0, width, height))
     physics.addBehavior(GravityBehavior2D(Vec2D(0, 0.5)))
 
@@ -30,13 +31,14 @@ def setup():
     particle_2 = Particle(width / 2 + length, 0, 8)
     particle_1.lock()  # Lock particle 1 in place.
 
-    # Must add particles to the world.
-    physics.addParticle(particle_1.p)
-    physics.addParticle(particle_2.p)
-    # (Particle is a Python wrapper, so pass its underlying Java object via .p)
-
     # Create one spring.
-    spring = VerletSpring2D(particle_1.p, particle_2.p, length, 0.01)
+    strength = 0.01  # How strong is the spring? (Higher value = more rigid)
+    spring = VerletSpring2D(particle_1._p, particle_2._p, length, strength)
+
+    # Must add everything to the world.
+    # (Particle is a Python wrapper, so pass its underlying Java object via ._p)
+    physics.addParticle(particle_1._p)
+    physics.addParticle(particle_2._p)
     physics.addSpring(spring)
 
 
@@ -54,6 +56,7 @@ def draw():
 
     # Move the particle according to the mouse.
     if is_mouse_pressed:
+        # First lock the particle, then set the x and y, then unlock() it.
         particle_2.lock()
         particle_2.x = mouse_x
         particle_2.y = mouse_y
@@ -62,18 +65,17 @@ def draw():
 
 # How cute is this simple Particle class?!
 class Particle():
-    """For py5, wrap Java VerletParticle2D as py5 cannot subclass it."""
+    """For py5, wrap Java VerletParticle2D as **py5 cannot subclass it**."""
 
-    def __init__(self, x: float, y: float, r: float) -> None:
+    def __init__(self, x: float, y: float, r: float):
         # A VerletParticle needs initial (x, y) position, but has no geometry ...
-        self.p = VerletParticle2D(x, y)  # Python wrapper forwards to this.
-        # so the r is used only for drawing.
-        self.r = r
+        self._p = VerletParticle2D(x, y)  # (Python wrapper forwards to this)
+        # ... so the r is used only for drawing.
+        self.r = r  
 
     def show(self) -> None:
         fill(127)
         stroke(0)
-        # When it comes time to draw particle, (x, y) is stored in self.particle.
         circle(self.x, self.y, self.r * 2)
 
 
@@ -82,12 +84,12 @@ class Particle():
     # Provide JS-style fields and forward the relevant physics verbs;
     # else Python would just create wrapper attributes and nothing would move.
     @property
-    def x(self) -> float: return float(self.p.x())
+    def x(self) -> float: return float(self._p.x())
     @x.setter
-    def x(self, v: float): self.p.set(float(v), self.y)
+    def x(self, v: float): self._p.set(float(v), self.y)
     @property
-    def y(self) -> float: return float(self.p.y())
+    def y(self) -> float: return float(self._p.y())
     @y.setter
-    def y(self, v: float): self.p.set(self.x, float(v))
-    lock   = lambda self: self.p.lock()
-    unlock = lambda self: self.p.unlock()
+    def y(self, v: float): self._p.set(self.x, float(v))
+    lock   = lambda self: self._p.lock()
+    unlock = lambda self: self._p.unlock()
